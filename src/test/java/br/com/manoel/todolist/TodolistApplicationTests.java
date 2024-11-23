e2d75ac6d0aba07c7ecc4711dfb810545b7d47da
Manoel Lucio <manoellucio@Manoels-MacBook-Air.local>
2024-11-23 08:33:52 -0300

    API todolist

---
+++ src/test/java/br/com/manoel/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.manoel.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
